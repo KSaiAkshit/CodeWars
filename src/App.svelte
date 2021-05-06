a228232ddf246ac975eee33de339bfc24cac6ba9
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2021-05-06 22:20:51 +0530

    I am speed

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	import Box from "./components/wideBox.svelte";
+	import people from "./components/people";
+</script>
+
+<main>
+	<details>
+		<summary>Material</summary>
+		<div />
+	</details>
+
+	<article>
+		<h2>Jump to</h2>
+		<a href="#svelte">Team Svelte</a> <br />
+		<a href="#jekyll"> Team Jekyll</a>
+	</article>
+
+	{#each ["svelte", "jekyll"] as team}
+		<h1 id="svelte">Team {team}</h1>
+		{#each people[team] as peep}
+			<Box>
+				<div slot="lhs">
+					<iframe
+						title="Web Demo by {peep.name}"
+						src="./svelte/{peep.name}"
+						frameborder="0"
+					/>
+				</div>
+				<div slot="rhs"><strong>By</strong>: {peep.name}</div>
+			</Box>
+		{/each}
+	{/each}
+</main>
+
+<style>
+	main {
+		padding: 1em;
+		margin: 0 auto;
+	}
+	a,
+	a:visited,
+	a:hover {
+		color: #a8f;
+		text-decoration: none;
+	}
+</style>
---
+++ src/components/wideBox.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let url;
+</script>
+
+<a href={url} class="section">
+    <div class="lhs">
+        <slot name="lhs" />
+    </div>
+    <div class="rhs">
+        <slot name="rhs" />
+        <div class="button" style="position:absolute;bottom:20px;right:15px;">
+            See More &rarr;
+        </div>
+    </div>
+</a>
+
+<style>
+    .section {
+        display: flex;
+        border: 1px solid #8888;
+        border-radius: 2rem;
+        height: 300px;
+        margin: 10px 0;
+    }
+    .lhs {
+        height: 100%;
+        width: 50%;
+    }
+    .rhs {
+        height: calc(100% - 4rem);
+        width: calc(50% - 4rem);
+        padding: 2rem;
+        position: relative;
+    }
+</style>
