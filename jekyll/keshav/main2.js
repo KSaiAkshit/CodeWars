e665db53f84cd168c0bf338bbb544db0c66b23bb
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2021-06-09 18:37:04 +0530

    Suggestions

--- jekyll/keshav/main2.js
+++ jekyll/keshav/main2.js
@@ -1,108 +1,104 @@
+window.onscroll = function () { scrollFunction() };
+// Pass directly ^
+// window.onscroll = scrollFunction;
 
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("nav").style.padding = "7px 0px 7px 10px";
+function scrollFunction () {
+  if ( document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ) {
+    document.getElementById( "nav" ).style.padding = "7px 0px 7px 10px";
 
   } else {
-    document.getElementById("nav").style.padding = "17px 0px 17px 10px";
+    document.getElementById( "nav" ).style.padding = "17px 0px 17px 10px";
 
   }
 }
+// SUGGESTION FOR ^, refer to 'Ternery Operator'
+// function scrollFunction () {
+//   document.getElementById( "nav" ).style.padding =
+//     ( document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ) ?
+//       "7px 0px 7px 10px" :
+//       "17px 0px 17px 10px";
+// }
 
-document.addEventListener("DOMContentLoaded", function( ) { 
-    const sections = 
- [document.querySelector(".homeS"),
-  document.querySelector(".aboutS"),
-  document.querySelector(".contactS")];
-
-
-  const navB =
- {homeS : document.querySelector("#aa"),
-  aboutS : document.querySelector("#bb"),
-  contactS : document.querySelector("#cc")};
-
-const options={
-	root: null,
-	threshold : 0.6,
-};
-
-const observer = new IntersectionObserver(function(entries,observer)
-{
- entries.forEach(entry =>{
- 			if(entry.isIntersecting)
- 			{     const nav = navB[entry.target.id];
-                // add 'active' class on the navItem
-						nav.classList.add("active");
-
-
- 	             
- 	           	        }
-
-
- });
-
-
-},options);
-
-sections.forEach(section => {
- observer.observe(section);
-});
-
-
-
-
-});
-
-const sections = 
- [document.querySelector(".homeS"),
-  document.querySelector(".aboutS"),
-  document.querySelector(".contactS")];
+// Look up 'window.onload', event listeners remain even after they are not needed
+document.addEventListener( "DOMContentLoaded", function () {
+  const sections =
+    [ document.querySelector( ".homeS" ),
+    document.querySelector( ".aboutS" ),
+    document.querySelector( ".contactS" ) ];
 
 
   const navB =
- {homeS : document.querySelector("#aa"),
-  aboutS : document.querySelector("#bb"),
-  contactS : document.querySelector("#cc")};
+  {
+    homeS: document.querySelector( "#aa" ),
+    aboutS: document.querySelector( "#bb" ),
+    contactS: document.querySelector( "#cc" )
+  };
 
-const options={
-	root: null,
-	threshold : 0.5,
+  const options = {
+    root: null,
+    threshold: 0.6,
+  };
+
+  const observer = new IntersectionObserver( function ( entries, observer ) {
+    entries.forEach( entry => {
+      if ( entry.isIntersecting ) {
+        const nav = navB[ entry.target.id ];
+        // add 'active' class on the navItem
+        nav.classList.add( "active" );
+      }
+    } );
+
+
+  }, options );
+
+  sections.forEach( section => {
+    observer.observe( section );
+  } );
+
+
+
+
+} );
+
+const sections =
+  [ document.querySelector( ".homeS" ),
+  document.querySelector( ".aboutS" ),
+  document.querySelector( ".contactS" ) ];
+
+
+const navB =
+{
+  homeS: document.querySelector( "#aa" ),
+  aboutS: document.querySelector( "#bb" ),
+  contactS: document.querySelector( "#cc" )
 };
 
-const observer = new IntersectionObserver(function(entries,observer)
-{
- entries.forEach(entry =>{
- 			if(entry.isIntersecting)
- 			{     const nav = navB[entry.target.id];
-                // add 'active' class on the navItem
-						nav.classList.add("active");
+const options = {
+  root: null,
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver( function ( entries, observer ) {
+  entries.forEach( entry => {
+    if ( entry.isIntersecting ) {
+      const nav = navB[ entry.target.id ];
+      // add 'active' class on the navItem
+      nav.classList.add( "active" );
 
 
-		}
+    }
 
-      Object.values(navB).forEach((item) => {
-        if (item != navB) {
-          item.classList.remove('active');
-        }
-      });
+    Object.values( navB ).forEach( ( item ) => {
+      if ( item != navB ) {
+        item.classList.remove( 'active' );
+      }
+    } );
 
- });
-
-
-},options);
-
-sections.forEach(section => {
- observer.observe(section);
-});
+  } );
 
 
+}, options );
 
-
-
-
-
-
- 
+sections.forEach( section => {
+  observer.observe( section );
+} );
